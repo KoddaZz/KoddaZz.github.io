7c8d306823e36e8e1207dbb7aeac96c7992d7c1d
FUSELIER Jules <jules.fuselier@etu.univ-amu.fr>
2025-01-08 21:52:10 +0100

    amelioration du form en cours...

--- script/contact.js
+++ script/contact.js
@@ -20,3 +20,30 @@ function toggleDropdown(element) {
       submenu.style.display = "block";
   }
 }
+
+function sendEmail(event) {
+  event.preventDefault(); // contre l'envoi du formulaire sans rien basique ( on aime pas ça nous )
+
+  // Pick toutes les valeurs des champs du form
+  const prenom = document.getElementById('textprenom').value;
+  const nom = document.getElementById('textnom').value;
+  const email = document.getElementById('textemail').value;
+  const note = document.getElementById('note').value;
+  const commentaires = document.getElementById('commentaires').value;
+
+  // creation du contenu de l'email
+  const subject = `Avis sur le site Dune - Par ${prenom} ${nom}`;
+  const body = `Prénom: ${prenom}\nNom: ${nom}\nEmail: ${email}\nNote: ${note}\nAvis:\n${commentaires}`;
+  
+  // encodage des valeurs afin de pas créer d'erreur au moment de l'envoi
+  const mailtoLink = `mailto:jules.fuselier@etu.univ-amu.fr?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+  
+/*      AIDE ( PENSE BETE ) :
+encodeURIComponent() est une fonction JavaScript qui transforme une chaîne de caractères en une version sécurisée pour une URL. 
+Elle remplace les caractères spéciaux (comme espaces, accents, &, ?, etc.) par leur équivalent encodé en pourcentage. 
+Cela garantit que ces caractères ne perturbent pas la structure de l'URL.
+*/
+
+  // client mail
+  window.location.href = mailtoLink;
+}
